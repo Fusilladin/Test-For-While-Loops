99e4a193e063b3340d84acde34eb83df47761892
Fusilladin <91526142+Fusilladin@users.noreply.github.com>
2021-09-29 18:36:04 -0500

    Add files via upload

---
+++ Test-For-While-Loops.py
@@ -0,0 +1,32 @@
+# nums = [1,2,3,4,5,]
+
+# Regular loop
+#for num in nums:
+#    if num == 3:
+#        print("Found!, Let's Go Again!\n")
+#        break
+#    print(num)
+
+# Regular loop
+#for num in nums:
+#    if num == 3:
+#        print("Found!, Keep Going!")
+#        continue
+#    print(num)
+
+# Sequential list prints every possible outcome
+#for num in nums:
+#    for letter in "abc":
+#        print(num, letter)
+
+# Print loop (1)0 times
+# for i in range(1, 11):
+#     print(i)
+
+# x = 0
+
+# while True:
+#     if x == 5:
+#         break
+#     print(x)
+#     x += 1
